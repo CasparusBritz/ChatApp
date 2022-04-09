3c9fc79cd57fcf4f872271cdca364c45b06cce7d
casparus.britz@gmail.com <casparus.britz@gmail.com>
2022-04-09 15:42:26 +0200

    Add project files.

---
+++ ChatApp/Server/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApp.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string message, string userName)
+        {
+           await Clients.All.SendAsync("SendMessage", userName, message);
+        }
+
+    }
+}
