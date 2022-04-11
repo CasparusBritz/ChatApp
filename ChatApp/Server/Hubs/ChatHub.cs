6d9edfce544a34d1a74ccfd4a7ed04799b8c3693
casparus.britz@gmail.com <casparus.britz@gmail.com>
2022-04-11 21:19:45 +0200

    Display usernames in chat bubble and display online users in box.

--- ChatApp/Server/Hubs/ChatHub.cs
+++ ChatApp/Server/Hubs/ChatHub.cs
@@ -1,41 +1,46 @@
 ﻿using ChatApp.Shared;
 using Microsoft.AspNetCore.SignalR;
+using System.Collections.Concurrent;
 
 namespace ChatApp.Server.Hubs
 {
     public class ChatHub : Hub
     {
+        static ConcurrentDictionary<string, User> connectedUsers = new ConcurrentDictionary<string, User>();
         public async Task SendMessage(string message, string userName)
         {
+
             await Clients.All.SendAsync("SendMessage", userName, message);
         }
 
         public async Task SendUsername(string userName, string connectionID)
         {
-            foreach (var userConn in ConnectedUsers.UserConnections)
+
+            if (!connectedUsers.Keys.Any(x => x == Context.ConnectionId))
             {
-                if (userConn.Key == connectionID)
-                {
-                    userConn.Value.UserName = userName;
-                }
+                connectedUsers.TryAdd(Context.ConnectionId, new User { UserName = userName });
             }
 
-            await Clients.All.SendAsync("RefreshConnections",userName);
+            
+            await Clients.All.SendAsync("RefreshUserNames", connectedUsers.Values.Select(x=>x.UserName));
         }
 
         public override async Task OnConnectedAsync()
         {
-            await base.OnConnectedAsync();
 
-            ConnectedUsers.UserConnections.Add(Context.ConnectionId, new());
+            await Clients.All.SendAsync("RefreshUserNames", connectedUsers.Values.Select(x => x.UserName));
+            await base.OnConnectedAsync();
         }
 
         public override async Task OnDisconnectedAsync(Exception e)
         {
+
+            connectedUsers.TryRemove(Context.ConnectionId, out _);
+
+            await Clients.All.SendAsync("RefreshUserNames", connectedUsers.Values.Select(x => x.UserName));
             await base.OnDisconnectedAsync(e);
-            ConnectedUsers.UserConnections.Remove(Context.ConnectionId);
         }
-      
+
 
     }
 }
